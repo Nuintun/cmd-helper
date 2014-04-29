6f1c8a4dee905b988e90e36d852caee19e3937ff
Nuintun <qianduanzu@gmail.com>
2014-04-29 14:58:00 +0800

    update travies

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 /**
  * Created by Newton on 2014/4/25.
  */
-var Tokens = require('./lib/tokens'),
-    TokenType = require('./lib/token-type');
-
-module.exports = {
-    Parse: function (css){
-        return new Tokens(css);
-    },
-    TokenType: TokenType
-};
